6e5c97a8eb14e228f9edc263b661da7820f98e17
Vova <white_net@mail.ru>
2026-02-22 13:19:15 +0000

    изменил 5

--- lnx_basics/section-2/5_chng_promt.sh
+++ lnx_basics/section-2/5_chng_promt.sh
@@ -1,3 +1,4 @@
 #!/bin/bash
 # Изменить prompt для пользователя max
-echo "PS1='[\d][\t][\u][\@][\h][\$ ]'" | sudo tee -a /home/max/.profile
+echo "PS1='[\D{%a %b %d}]\u@\h\$ '" | sudo tee -a /home/max/.profile
+cp /home/max/.profile /home/vova/purpleshchoolhw/lnx_basics/section-2/profile
