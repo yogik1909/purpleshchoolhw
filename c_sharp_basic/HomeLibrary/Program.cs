8f2ff98a5e64057fca59cc888dc8f9cc68572b80
Vova <white_net@mail.ru>
2026-04-28 14:38:40 +1000

    Ввод библиотеки

---
+++ c_sharp_basic/HomeLibrary/Program.cs
@@ -0,0 +1,21 @@
+﻿// Объявите переменные соответствующего типа для хранения названия, автора, года издания, ISBN
+
+// Реализуйте ввод данных о книге с помощью метода Console.ReadLine() в объявленные ранее переменные
+
+// Реализуйте вывод введённых ранее данных о книге с помощью метода Console.WriteLine()
+
+
+
+Console.WriteLine("Введите название книги: ");
+string title = Console.ReadLine();
+
+Console.WriteLine("Введите автора книги: ");
+string author = Console.ReadLine();
+
+Console.WriteLine("Введите год издания книги: ");
+string year = Console.ReadLine();
+
+Console.WriteLine("Введите ISBN книги: ");
+string isbn = Console.ReadLine();
+
+Console.WriteLine($"Название книги: {title} Автор: {author} Год издания: {year} ISBN: {isbn}");
