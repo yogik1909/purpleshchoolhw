301dfe5323e204c68bb7ccb22417d5d347642ad5
Vova <white_net@mail.ru>
2026-05-05 00:20:34 +1000

    Типизация

---
+++ typescript/2-typing/local-modules.d.ts
@@ -0,0 +1,14 @@
+declare module './makeOrdinal' {
+    function makeOrdinal(words: string): string;
+    export = makeOrdinal;
+}
+
+declare module './isFinite' {
+    function isFinite(value: number): boolean;
+    export = isFinite;
+}
+
+declare module './isSafeNumber' {
+    function isSafeNumber(value: number): boolean;
+    export = isSafeNumber;
+}
